faf6404a8a1fe97f46d32d8a1def144afcadb0c0
meyron1456 <meyron1456@gmail.com>
2022-12-12 23:12:00 +0200

    all client-side without http requests

--- client/src/components/ProductList/ProductList.tsx
+++ client/src/components/ProductList/ProductList.tsx
@@ -1,18 +1,21 @@
-import { Link } from "react-router-dom";
 import { Product } from "../../models/product";
 import ProductCard from "../ProductCard/ProductCard";
 import "./ProductList.scss";
-import { products } from "../../data/products";
 
-const ProductList = (): JSX.Element => {
+interface ProductListProps {
+    onClick: (product:Product) => void
+    products: Product[]
+}
+
+const ProductList = ({ onClick, products } : ProductListProps): JSX.Element => {
     return (
-        <div className="product-list">
-            {products.map(product => 
-            <div className="product-card-item">
-                <Link to={`/${product.name}`} style={{color: '#0D0D0D', textDecoration: 'none'}}>
-                    <ProductCard product={product}></ProductCard>
-                </Link>
-            </div>)}
+        <div className="product-list-component">
+            <div className="product-list">
+                {products.map(product => 
+                <div className="product-card-item" onClick={() => onClick(product)}>
+                    <ProductCard product={product} ></ProductCard>
+                </div>)}
+            </div>
         </div>
     );
 } 
--- client/src/data/products.ts
+++ client/src/data/products.ts
@@ -3,17 +3,11 @@ import { Product } from "../models/product";
 const products: Product[] = [{
     name: "necklace",
     imageUrl: "/necklace.jpeg",
-    price: 1000,
-    storeName: "Tanda Crafts inc.",
-    secondImageUrl: "/necklace.webp",
-    description: "the bex necklace ever!"
+    price: 1000
   }, {
     name: "bracelet",
     imageUrl: "/bracelet.jpeg",
-    price: 300,
-    storeName: "Tanda Crafts FOREVER",
-    secondImageUrl: "/bracelet.png",
-    description: "the bex bracelet ever!"
+    price: 300
 }];
 
 export { products };
